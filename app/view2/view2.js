92daf429a437104f2b7fc3cece43d8bdbb5fd24f
doomruin <doomruin@github.com>
2015-03-15 14:29:56 +0800

    heiehi

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -11,12 +11,23 @@ angular.module('myApp.view2', ['ngRoute'])
 
 .controller('View2Ctrl', ['$scope','$http','$location',function($scope, $http, $location) {
 
+        $scope.showRecommend=false;
+        $scope.showRec = function(keyword) {
+            $scope.showRecommend=true;
+
+        };
+
         $http.get('json/videoDetail.json').success(function(result) {
             $scope.video = result;
         });
         $http.get('json/tags.json').success(function(tags) {
             $scope.tags = tags;
         });
+
+        $http.get('json/recommendation.json').success(function(recommends) {
+            $scope.recommends = recommends;
+        });
 }])
 
 ;
+
